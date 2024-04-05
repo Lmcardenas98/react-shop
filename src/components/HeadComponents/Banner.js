5d3bb5330c61ca3b1e63aaa11f9db0cc5b9e525f
lmcardenas98 <lmcardenas.981106@gmail.com>
2024-04-05 02:10:50 -0400

    new change for state

--- src/components/HeadComponents/Banner.js
+++ src/components/HeadComponents/Banner.js
@@ -1,37 +1,37 @@
-import banner from "../../media/banner.jpg";
 import useProviderCart from "../../hook/useProviderCart";
 import { useEffect, useState } from "react";
 
 export default function Banner() {
   const { cart, products } = useProviderCart();
-  const [item, setItem] = useState(null);
+  const [cont, setCont] = useState(0);
+  let item = products?.filter((item) => item.id <= 2);
 
   useEffect(() => {
-    const timeout = setInterval(() => {
-      const random = Math.floor(Math.random() * 10);
-      setItem(products?.filter((item) => item.id === random)[0]);
-      console.log(item);
-    }, 5000);
+    const timer = setTimeout(() => console.log("leo"), 1000);
   }, []);
-
-  // console.log(item);
-  return (
-    <div className="banner-container">
-      <div className="head-banner">
-        <div className="banner-item-description">
-          <h2>{item?.category}</h2>
-          <h1>Higth cuality {item?.title}</h1>
-          <h2>For only {item?.price}$</h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem I
-          </p>
-          <button className="header-btn">SHOW NOW</button>
-        </div>
-        <div className="banner-item-image">
-          <img className="banner-image" alt="item" src={item?.image}></img>
+  if (item) {
+    return (
+      <div className="banner-container">
+        <div className="head-banner">
+          <div className="banner-item-description">
+            <h2>{item[cont]?.category}</h2>
+            <h1>Higth cuality {item[cont]?.title}</h1>
+            <h2>For only {item[cont]?.price}$</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem I
+            </p>
+            <button className="header-btn">SHOW NOW</button>
+          </div>
+          <div className="banner-item-image">
+            <img
+              className="banner-image"
+              alt="item"
+              src={item[cont]?.image}
+            ></img>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
