bb6e9bc1699e00292e2adc7e235f2a7af65aba7e
lmcardenas98 <lmcardenas.981106@gmail.com>
2024-04-11 03:47:27 -0400

    added some animation

--- src/components/HeadComponents/Banner.js
+++ src/components/HeadComponents/Banner.js
@@ -5,32 +5,31 @@ export default function Banner() {
   const { cart, products } = useProviderCart();
   const [cont, setCont] = useState(0);
   let item = products?.filter((item) => item.id <= 2);
-  useEffect(() => {
-    setInterval(() => {
-      let now = new Date();
-      if (now.getSeconds() % 15 === 1) {
-        setCont(1 - cont);
-      }
-    }, 1000);
-  });
 
   if (item) {
     return (
       <div className="banner-container">
-        <div className="head-banner">
-          <div className="banner-item-description">
-            <h2>{item[cont]?.category}</h2>
-            <h1>Higth cuality {item[cont]?.title}</h1>
-            <h2>For only {item[cont]?.price}$</h2>
-            <p>
+        <div className="head-banner banner1">
+          <div className="banner-item-description item1">
+            <h2
+              onAnimationIteration={() => setCont(1 - cont)}
+              className="category1"
+            >
+              {item[cont]?.category}
+            </h2>
+            <h1 className="title1">
+              Higth cuality {item[cont]?.title.slice(0, 10)}
+            </h1>
+            <h2 className="price1">For only {item[cont]?.price}$</h2>
+            <p className="description1">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem I
             </p>
-            <button className="header-btn">SHOW NOW</button>
+            <button className="header-btn btn1">SHOW NOW</button>
           </div>
-          <div className="banner-item-image">
+          <div className="banner-item-image image1 ">
             <img
-              className="banner-image"
+              className="banner-image image-tag1"
               alt="item"
               src={item[cont]?.image}
             ></img>
--- src/layouts/bodySections/SectionsDeals.js
+++ src/layouts/bodySections/SectionsDeals.js
@@ -1,22 +1,26 @@
-import Banner from "../../components/HeadComponents/Banner";
-import bannerImg from "../../media/banner.jpg";
+import { useEffect, useState } from "react";
+import useProviderCart from "../../hook/useProviderCart";
 
 export default function SectionDeals() {
-  return (
-    <div className="section-deals-container">
-      <div className="banner-container">
-        <div className="head-banner">
-          <div className="banner-item-description">
-            <h1>Deal of the day</h1>
-            <p>Get at discount price!</p>
-            <p>The countdown is finished</p>
-            <button className="header-btn">SHOP NOW</button>
-          </div>
-          <div className="deals-image ">
-            <img className="deals-img" src={bannerImg}></img>
+  const { products } = useProviderCart();
+  let img = products?.filter((item) => item.id === 3);
+  if (img) {
+    return (
+      <div className="section-deals-container">
+        <div className="banner-container">
+          <div className="head-banner">
+            <div className="deals-item-description">
+              <h1>Deal of the day</h1>
+              <p>Get at discount price!</p>
+              <p>The countdown is finished</p>
+              <button className="header-btn">SHOP NOW</button>
+            </div>
+            <div className="deals-image ">
+              <img className="deals-img" src={img[0]?.image}></img>;
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
